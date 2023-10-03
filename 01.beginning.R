f749997a079ee9d76b76cc01103b41cb4a53a9a3
1dharmaaaa <146854292+1dharmaaaa@users.noreply.github.com>
2023-10-03 17:52:21 +0200

    Update 01.beginning.R

--- 01.beginning.R
+++ 01.beginning.R
@@ -15,4 +15,29 @@ FINAL
 
 FINAL^2
 
+sophi <- c(10, 20,30,50,70) #microplastics
+#functions have parenthesis inside them there is R
+
+
+paula <- c(100,500, 600,1000,2000) #people
+
+plot (paula, sophie)
+
+plot (paula, sophie, xlab="number of people", Ylab="microoplastics)
+plot(paula, sophie, xlab="number of people", Ylab="microoplastics)
+
+ plot
+people <- paula
+microplastucs <- sophi
+ plot(people, microplastics)
+ plot(people, microplastics, pch=19)
+
+plot(people,microplastics, pch=19, cex=2)
+plot(people, microplastics, pch=19, cex=2, col=blue)
+
+
+
+
+
+
 
