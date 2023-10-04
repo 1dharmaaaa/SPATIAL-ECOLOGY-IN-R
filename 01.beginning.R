47631739bd40213f1d2bf9b1157e5e5224fe5ab9
1dharmaaaa <146854292+1dharmaaaa@users.noreply.github.com>
2023-10-04 18:50:17 +0200

    Update 01.beginning.R

--- 01.beginning.R
+++ 01.beginning.R
@@ -1,40 +1,40 @@
-#HERE I CAN WRITE ANYTHING I WANT.
+# Here I can write anything I want!!! 42 is the meaning of life univcerse and all!
 
-# R AS A CALCULATOR
-2+3
+# R as a calculator
+2 + 3
 
-# ASSIGN TO AN OBJECT
- ZIMA <- 2+3
- ZIMA
+# Assign to an object
+zima <- 2 + 3
+zima
 
-DUCCIO <-5+3
-DUCCIO
+duccio <- 5 + 3
+duccio
 
-FINAL <- ZIMA* DUCCIO
-FINAL
+final <- zima * duccio
+final
 
-FINAL^2
+final^2
 
-sophi <- c(10, 20,30,50,70) #microplastics
-#functions have parenthesis inside them there is R
+# array
+sophi <- c(10, 20, 30, 50, 70) # microplastics 
+# fcuntions have parentheses and inside them there are arguments
+
+paula <- c(100, 500, 600, 1000, 2000) # people
+
+plot(paula, sophi)
+
+plot(paula, sophi, xlab="number of people", ylab="microplastics")
 
 
-paula <- c(100,500, 600,1000,2000) #people
-
-plot (paula, sophie)
-
-plot (paula, sophie, xlab="number of people", Ylab="microoplastics)
-plot(paula, sophie, xlab="number of people", Ylab="microoplastics)
-
- plot
 people <- paula
-microplastucs <- sophi
- plot(people, microplastics)
- plot(people, microplastics, pch=19)
+microplastics <- sophi
 
-plot(people,microplastics, pch=19, cex=2)
-plot(people, microplastics, pch=19, cex=2, col=blue)
+plot(people, microplastics)
+plot(people, microplastics, pch=19)
+# https://www.google.com/search?client=ubuntu-sn&hs=yV6&sca_esv=570352775&channel=fs&sxsrf=AM9HkKknoSOcu32qjoErsqX4O1ILBOJX4w:1696347741672&q=point+symbols+in+R&tbm=isch&source=lnms&sa=X&ved=2ahUKEwia9brkm9qBAxVrQvEDHbEYDuMQ0pQJegQIChAB&biw=1760&bih=887&dpr=1.09#imgrc=lUw3nrgRKV8ynM
 
+plot(people, microplastics, pch=19, cex=2)
+plot(people, microplastics, pch=19, cex=2, col="blue")
 
 
 
