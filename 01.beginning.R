11670f7a86781693fee53e2de4ad57e1b8a19a62
1dharmaaaa <146854292+1dharmaaaa@users.noreply.github.com>
2023-10-08 16:50:11 +0200

    Update 01.beginning.R

--- 01.beginning.R
+++ 01.beginning.R
@@ -1,3 +1,4 @@
+# Here I can write anything I want!!! 42 is the meaning of life univcerse and all!
 
 # R as a calculator
 2 + 3
@@ -34,4 +35,3 @@ plot(people, microplastics, pch=19)
 
 plot(people, microplastics, pch=19, cex=2)
 plot(people, microplastics, pch=19, cex=2, col="blue")
-
