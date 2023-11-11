9e6787968240c12f258632994f2e493afc96f1be
1dharmaaaa <146854292+1dharmaaaa@users.noreply.github.com>
2023-11-11 16:48:48 +0100

    Update 02.populationdensity.R

--- 02.populationdensity.R
+++ 02.populationdensity.R
@@ -1,57 +1,62 @@
 # Code related to population ecology
 
-# A package is needed for point pattern analysis
+# Install and load the 'spatstat' package for point pattern analysis
 install.packages("spatstat")
 library(spatstat)
 
-# let's use the bei data:
-# data description:
-# https://CRAN.R-project.org/package=spatstat
-
+# Load the 'bei' dataset from the 'spatstat' package
+# Data description: https://CRAN.R-project.org/package=spatstat
 bei
 
-# plotting the data
+# Plot the 'bei' dataset
 plot(bei)
 
-# changing dimension - cex
+# Change point size using 'cex'
 plot(bei, cex=.2)
 
-# changing the symbol - pch
+# Change point symbol using 'pch'
 plot(bei, cex=.2, pch=19)
 
-# additional datasets
+# Additional datasets in 'bei.extra'
 bei.extra
+
+# Plot the 'bei.extra' dataset
 plot(bei.extra)
 
-# let's use only part of the dataset: elev
+# Use only part of the dataset: 'elev'
 plot(bei.extra$elev)
 elevation <- bei.extra$elev
 plot(elevation)
 
-# second method to select elements
+# Second method to select elements
 elevation2 <- bei.extra[[1]]
 plot(elevation2)
 
-# passing from points to a countinuous surface
+# Convert points to a continuous surface using 'density'
 densitymap <- density(bei)
 plot(densitymap)
 points(bei, cex=.2)
 
+# Create a color palette for density map
 cl <- colorRampPalette(c("black", "red", "orange", "yellow"))(100)
 plot(densitymap, col=cl)
 
+# Create a color palette with fewer colors
 cl <- colorRampPalette(c("black", "red", "orange", "yellow"))(4)
 plot(densitymap, col=cl)
 
+# Create a new color palette
 clnew <- colorRampPalette(c("dark blue", "blue", "light blue"))(100)
 plot(densitymap, col=clnew)
 
+# Plot the 'bei.extra' dataset
 plot(bei.extra)
 
-elev <- bei.extra[[1]] # bei.extra$elev
+# Access the 'elev' component from 'bei.extra'
+elev <- bei.extra[[1]]
 plot(elev)
 
-# multiframe
+# Create multiframe plots
 par(mfrow=c(1,2))
 plot(densitymap)
 plot(elev)
@@ -64,3 +69,5 @@ par(mfrow=c(1,3))
 plot(bei)
 plot(densitymap)
 plot(elev)
+
+
