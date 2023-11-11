814d13903eda18e59519a5673658693335f4eb05
1dharmaaaa <146854292+1dharmaaaa@users.noreply.github.com>
2023-11-11 16:53:59 +0100

    Update 04.remotesensing.visualisation2.R

--- 04.remotesensing.visualisation2.R
+++ 04.remotesensing.visualisation2.R
@@ -1,17 +1,21 @@
 # Remote sensing for ecosystem monitoring
 
-
+# Install the 'devtools' and 'terra' packages if not installed
 # install.packages("devtools")
 # install.packages("terra")
 
+# Load the 'devtools' and 'terra' packages
 library(devtools)
 library(terra)
 
+# Install the 'imageRy' package from GitHub using 'devtools'
 devtools::install_github("ducciorocchini/imageRy")
 
+# Load the 'imageRy' package
 library(imageRy)
 
+# List available image files
 im.list()
 
+# Import the blue band from Sentinel-2 (band 2)
 b2 <- im.import("sentinel.dolomites.b2.tif")
-
