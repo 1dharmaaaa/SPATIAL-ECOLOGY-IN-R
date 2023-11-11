a46c8f731579a63a7acd82b8df5c799d76c8472b
1dharmaaaa <146854292+1dharmaaaa@users.noreply.github.com>
2023-11-11 16:56:56 +0100

    Update 05.spectral.indices_temp.R

--- 05.spectral.indices_temp.R
+++ 05.spectral.indices_temp.R
@@ -1,23 +1,22 @@
-# Indices derived from RS imagery
-
+# Load necessary libraries
 library(imageRy) # beloved package developed at unibo
 library(terra)
 library(ggplot2)
 library(viridis)
 
+# List available image files
 im.list()
 
+# Import the 1992 image
 m1992 <- im.import("matogrosso_l5_1992219_lrg.jpg")
 
-# layer 1: NIR
-# layer 2: red
-# layer 3: green
-
+# Define band layers for RGB combinations
+# layer 1: NIR, layer 2: red, layer 3: green
 im.plotRGB.user(m1992, 1, 2, 3)
 im.plotRGB.user(m1992, 2, 1, 3)
 im.plotRGB.user(m1992, 2, 3, 1)
 
-# import the 2006 image
+# Import the 2006 image
 m2006 <- im.import("matogrosso_ast_2006209_lrg")
 
 # Exercise: plot the 2006 image with NIR in the Green component of the RGB space
@@ -32,37 +31,30 @@ im.plotRGB.user(m1992, 1, 2, 3)
 im.plotRGB.user(m2006, 1, 2, 3)
 
 # Calculating the DVI
-# dvi1992
+# DVI for 1992
 dvi1992 = m1992[[1]] - m1992[[2]]
-dvi1992
-
-cl <- colorRampPalette(c("darkblue", "yellow", "red", "black"))(100) # specifying a color scheme
 plot(dvi1992, col=cl)
 
-# dvi2006
+# DVI for 2006
 dvi2006 = m2006[[1]] - m2006[[2]]
-dvi2006
-
-cl <- colorRampPalette(c("darkblue", "yellow", "red", "black"))(100) # specifying a color scheme
 plot(dvi2006, col=cl)
 
-# dvi by imageRy
+# DVI calculation using imageRy
 dvi1992i <- im.dvi(m1992, 1, 2)
 dvi2006i <- im.dvi(m2006, 1, 2)
 
-# ndvi
+# NDVI calculation
 ndvi1992 = (m1992[[1]] - m1992[[2]]) / (m1992[[1]] + m1992[[2]])
 ndvi2006 = (m2006[[1]] - m2006[[2]]) / (m2006[[1]] + m2006[[2]])
 
+# Turn off graphics device
 dev.off()
+
+# Multiframe plot for NDVI
 par(mfrow=c(1,2))
 plot(ndvi1992, col=cl)
 plot(ndvi2006, col=cl)
 
-# scientifically meaningful image for everyone!
-clvir <- colorRampPalette(c("violet", "dark blue", "blue", "green", "yellow"))(100) # specifying a color scheme
+# Scientifically meaningful image for everyone!
+clvir <- colorRampPalette(c("violet", "dark blue", "blue", "green", "yellow"))(100)
 plot(ndvi2006, col=clvir)
-
-
-
-
