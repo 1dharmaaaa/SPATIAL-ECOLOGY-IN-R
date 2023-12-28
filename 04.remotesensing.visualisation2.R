3d8cb0788a7209c0d3f29b0e575d1c294d84b972
VaishnaviPittala <146854292+1dharmaaaa@users.noreply.github.com>
2023-12-29 00:33:31 +0100

    Update 04.remotesensing.visualisation2.R

--- 04.remotesensing.visualisation2.R
+++ 04.remotesensing.visualisation2.R
@@ -1,4 +1,4 @@
-# Comment: In this code snippet, the necessary packages are installed and loaded for remote sensing analysis. 
+# In this code snippet, the necessary packages are installed and loaded for remote sensing analysis. 
 # 'devtools' and 'terra' packages are installed if not already installed. 
 # The 'imageRy' package is installed from GitHub using 'devtools', and then it is loaded.
 
@@ -10,25 +10,25 @@
 library(devtools)
 library(terra)
 
-# Comment: The 'devtools' and 'terra' packages are loaded for package installation and spatial data handling.
+#  The 'devtools' and 'terra' packages are loaded for package installation and spatial data handling.
 
 # Install the 'imageRy' package from GitHub using 'devtools'
 devtools::install_github("ducciorocchini/imageRy")
 
-# Comment: The 'imageRy' package is installed from GitHub using 'devtools'.
+# The 'imageRy' package is installed from GitHub using 'devtools'.
 
 # Load the 'imageRy' package
 library(imageRy)
 
-# Comment: The 'imageRy' package is loaded for remote sensing analysis.
+# The 'imageRy' package is loaded for remote sensing analysis.
 
 # List available image files
 im.list()
 
-# Comment: A list of available image files is displayed.
+#  A list of available image files is displayed.
 
 # Import the blue band from Sentinel-2 (band 2)
 b2 <- im.import("sentinel.dolomites.b2.tif") 
 
-# Comment: The blue band from Sentinel-2 (band 2) is imported and assigned to the variable 'b2'.
+#  The blue band from Sentinel-2 (band 2) is imported and assigned to the variable 'b2'.
 
